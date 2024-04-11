591296d2c45eac06cc84b0b230ad431636b0d4c0
khushi kesharwani <149566530+Khushi24kes@users.noreply.github.com>
2024-04-11 14:24:06 +0530

    commit

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,16 @@
+// App.js
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="parent">
+      <div className="child">Child 1</div>
+      <div className="child">Child 2</div>
+      <div className="child">Child 3</div>
+    </div>
+  );
+}
+
+export default App;
